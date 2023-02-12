f7b01cd21eeefd9b4aab50ece54ade4f355b7510
Luke Jang <h.lukejang@gmail.com>
2023-02-12 14:41:11 -0500

    2023-02-12

---
+++ algorithms/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        substring = ''
+        maxLength = 0
+
+        for char in s:
+
+            if char not in substring: 
+                substring += char
+            else:
+                substring = substring[char.index()+1:]
+                substring += char
+                
+            
+            if len(substring) > maxLength:
+                maxLength = len(substring)
+
+        return maxLength
