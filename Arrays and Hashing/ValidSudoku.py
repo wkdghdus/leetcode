92e694cb2aeba70acf92ab8114dd0685b0d788fe
Luke Jang <h.lukejang@gmail.com>
2024-01-21 22:32:29 -0500

    36. Valid Sudoku

--- Arrays and Hashing/ValidSudoku.py
+++ Arrays and Hashing/ValidSudoku.py
@@ -23,15 +23,15 @@ class Solution(object):
                     if board[row][column] != ".":
                         return False
             
-        for boxrow in range(0, 9, 3):
-            for boxcolumn in range(0, 9, 3):
+        for boxRow in range(0, 9, 3):
+            for boxColumn in range(0, 9, 3):
                 nums = []
                 for row in range(0, 3):
                     for column in range(0, 3):
-                        if board[row+boxrow][column+boxcolumn] not in nums:
-                            nums.append(board[row+boxrow][column+boxcolumn])
+                        if board[row+boxRow][column+boxColumn] not in nums:
+                            nums.append(board[row+boxRow][column+boxColumn])
                         else:
-                            if board[row+boxrow][column+boxcolumn] != ".":
+                            if board[row+boxRow][column+boxColumn] != ".":
                                 return False
             
         return True
