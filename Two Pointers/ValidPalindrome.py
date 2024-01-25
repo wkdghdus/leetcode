303cb9299acb8a32dda52d06908a87b9da4f9788
Luke Jang <h.lukejang@gmail.com>
2024-01-25 16:52:50 -0500

    125. Valid Palindrome

--- Two Pointers/ValidPalindrome.py
+++ Two Pointers/ValidPalindrome.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-        s = s.lower()
+        
         for letter in s:
             if not letter.isalnum():
                 s = s.replace(letter, '')
+
+        return s == s[::-1]
         
-        for i in range(len(s)/2):
-            if s[i] != s[len(s)-i-1]:
-                return False
+        # for i in range(len(s)/2):
+        #     if s[i] != s[len(s)-i-1]:
+        #         return False
         
-        return True
+        # return True
