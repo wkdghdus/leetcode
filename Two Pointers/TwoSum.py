e54be34c5030d5025571dafc103e4bfac24e3468
Luke Jang <h.lukejang@gmail.com>
2024-01-27 23:44:51 -0500

    167. Two Sum II - Input Array Is Sorted

---
+++ Two Pointers/TwoSum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+    # make a triangle using "*" and for loop
+    for i in range(1, 6):
+        print("*" * i)   
