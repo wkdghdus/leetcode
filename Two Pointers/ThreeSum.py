132b35fed9611b8fe7bd9b58cf3315e8432c4c6f
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-05-03 16:46:05 -0400

    15. 3Sum

--- Two Pointers/ThreeSum.py
+++ Two Pointers/ThreeSum.py
@@ -4,30 +4,60 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
-        res = []
+
+        ###second try
+        #first sort the array
         nums.sort()
 
+        #initialize the result array
+        res = []
+
         for i, a in enumerate(nums):
-            # Skip positive integers
-            if a > 0:
-                break
-
-            if i > 0 and a == nums[i - 1]:
+            if i > 0 and a == nums[i-1]:
                 continue
+            
+            left = i + 1
+            right = len(nums)-1
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+            while left < right:
+                sum = a + nums[left] + nums[right]
+
+                if sum > 0:
+                    right -= 1
+                elif sum < 0:
+                    left += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-                        
+                    res.append([a, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left-1] and left < right:
+                        left += 1
+
         return res
+                
+
+        ###first try but too slow!!!
+        # #first sort the array
+        # nums.sort()
+
+        # #initialize the result array
+        # res = []
+
+        # for targetInd in range(len(nums)):
+        #     left = targetInd + 1
+        #     right = len(nums)-1
+
+        #     while left < right:
+        #         if nums[left] + nums[right] == nums[targetInd]*-1 and [nums[targetInd], nums[left], nums[right]] not in res:
+        #             res.append([nums[targetInd], nums[left], nums[right]])
+        #             left += 1
+        #             right -= 1
+        #         elif nums[left] + nums[right] < nums[targetInd]*-1:
+        #             left += 1
+        #         else:
+        #             right -= 1
+                
+        # return res
+
+
+
+        
