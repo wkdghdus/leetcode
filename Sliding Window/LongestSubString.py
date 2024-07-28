81de5e0982643c8d8e5cda3ae98335037ad30b26
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-07-28 19:00:52 -0400

    3. Longest Substring Without Repeating Characters

--- Sliding Window/LongestSubString.py
+++ Sliding Window/LongestSubString.py
@@ -4,21 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        #declare set to save the non duplicating characters (can be any array like data structure)
-        charSet = set()
-        l = 0   #left pointer
-        res = 0 #result
-
-        #iterate through the string, r is the right pointer
-        for r in range(len(s)):
-            #while the r is in the set, reduce the window by removing the left most character 
-            while s[r] in charSet:
-                charSet.remove(s[l]) #remove the left most character
-                l += 1  #move the left pointer to the right
-            #add the character to the set
-            charSet.add(s[r])
-            #get the max length of the window
-            res = max(res, r - l + 1)
-
         
-        return res
+        subst = []
+        longest = 0
+
+        for i in range(len(s)):
+            while s[i] in subst:
+                subst.pop(0)
+            
+            subst.append(s[i])
+
+            longest = max(longest, len(subst))
+
+        return longest
+            
