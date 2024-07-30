9156629eaa85af0132e92e0538a798ea59d12519
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-07-30 15:57:21 -0400

    424. Longest Repeating Character Replacement

--- Sliding Window/LongestRepeating.py
+++ Sliding Window/LongestRepeating.py
@@ -5,27 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        #declare a dictionary to save the count of the characters within the window
-        count = {}
         
-        #left pointer
+        #dictionary for the frequency
+        freq = {}
         l = 0
-        #max frequency
-        maxf = 0
+        r = 1
+        res = 0
+        maxFreq = 0
 
-        #go through the string, r is the right pointer
         for r in range(len(s)):
+            freq[s[r]] = 1 + freq.get(s[r], 0)
+            maxFreq = max(maxFreq, freq[s[r]])
 
-            #use the letter of the right pointer as the key and increment the value 
-            count[s[r]] = 1 + count.get(s[r], 0)
-            #change the max frequency accordingly
-            maxf = max(maxf, count[s[r]])
-
-            #if the length of the window minus the max frequency = the letters that must be changed
-            #if that count is bigger the possible operation that we can perform then reduce the window
-            #and reduce the count of the frequency of the left pointer value
-            if (r - l + 1) - maxf > k:
-                count[s[l]] -= 1
+            if (r-l+1) - maxFreq > k: 
+                freq[s[l]] -= 1
                 l += 1
+            
+            res = max(res, r-l+1)
 
-        return (r - l + 1)
+        return res
+            
+        
