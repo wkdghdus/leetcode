0e286aef84fb57cd0b0749f758781729cda1fc47
Hoyeon Luke Jang <hjang55@uwo.ca>
2024-08-03 12:03:36 -0400

    143. Reorder List

--- Linked List/ListNode.py
+++
@@ -1,4 +0,0 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
--- Linked List/ReorderList.py
+++ Linked List/ReorderList.py
@@ -1,21 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution(object):
     def reorderList(self, head):
         """
         :type head: ListNode
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        
-        # find middle
+
+        #find middle
         slow, fast = head, head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
+        
         # reverse second half
         second = slow.next
         prev = slow.next = None
@@ -24,7 +26,7 @@ class Solution(object):
             second.next = prev
             prev = second
             second = tmp
-
+        
         # merge two halfs
         first, second = head, prev
         while second:
