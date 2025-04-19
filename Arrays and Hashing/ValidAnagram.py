7dc4c7021a209cda33e60228f854311cc8333f26
Hoyeon Luke Jang <h.lukejang@gmail.com>
2025-04-19 18:33:15 -0400

    242. Valid Anagram (reviewed)

--- Arrays and Hashing/ValidAnagram.py
+++ Arrays and Hashing/ValidAnagram.py
@@ -5,35 +5,47 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
-        #create a dictionary to store the number of times each character appears in s
-        sDict = {}
-        tDict = {}
-        for char in s:
-            if char in sDict:
-                sDict[char] += 1
-            else:
-                sDict[char] = 1
-        
-        #create a dictionary to store the number of times each character appears in t
-        for char in t:
-            if char in tDict:
-                tDict[char] += 1
-            else:
-                tDict[char] = 1
-        
-        #check if the dictionaries are the same
-        
-        if len(sDict) != len(tDict):
+
+        if len(s) != len(t):
             return False
-        
-        for key in sDict:
-            if key not in tDict:
-                return False
-            elif sDict[key] != tDict[key]:
+
+        for c in set(s):
+            if s.count(c) != t.count(c):
                 return False
         
         return True
+
+        ###sorting
+        #return sorted(s) == sorted(t)
+        
+        # #create a dictionary to store the number of times each character appears in s
+        # sDict = {}
+        # tDict = {}
+        # for char in s:
+        #     if char in sDict:
+        #         sDict[char] += 1
+        #     else:
+        #         sDict[char] = 1
+        
+        # #create a dictionary to store the number of times each character appears in t
+        # for char in t:
+        #     if char in tDict:
+        #         tDict[char] += 1
+        #     else:
+        #         tDict[char] = 1
+        
+        # #check if the dictionaries are the same
+        
+        # if len(sDict) != len(tDict):
+        #     return False
+        
+        # for key in sDict:
+        #     if key not in tDict:
+        #         return False
+        #     elif sDict[key] != tDict[key]:
+        #         return False
+        
+        # return True
 
         # #check if each unique character in s and t are the same
         # for char in s:
