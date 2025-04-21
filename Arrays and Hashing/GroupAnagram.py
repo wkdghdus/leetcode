5b9f4637fa5acb8f543170fe7cb55a7e3accad1d
Hoyeon Luke Jang <h.lukejang@gmail.com>
2025-04-20 21:29:58 -0400

    49. Group Anagrams (reviewed)

--- Arrays and Hashing/GroupAnagram.py
+++ Arrays and Hashing/GroupAnagram.py
@@ -1,41 +1,20 @@
 class Solution(object):
 
-    # def toDict(self, word):
-    #     wordDict = [0] * 26
-
-    #     for letter in word:
-    #         wordDict[ord(letter) - ord("a")] += 1
-            
-    #     return wordDict
-
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
 
-        retHash = {}
+        anagram_dict = {}
 
         for word in strs:
-            sortedWord = "".join(sorted(word))
 
-            if sortedWord in retHash:
-                retHash[sortedWord].append(word)
+            sorted_word = "".join(sorted(word))
+
+            if sorted_word in anagram_dict:
+                anagram_dict[sorted_word].append(word)
             else:
-                retHash[sortedWord] = [word]   
-
-        return retHash.values()
-
-    #     anagramHash = {}
-
-    #     for word in strs:
-    #         wordDict = self.toDict(word)
-
-    #         if tuple(wordDict) in anagramHash:
-    #             anagramHash[tuple(wordDict)].append(word)
-    #         else:
-    #             anagramHash[tuple(wordDict)] = [word]
-            
-    #     return anagramHash.values()
-
-    
+                anagram_dict[sorted_word] = [word]
+        
+        return anagram_dict.values()
