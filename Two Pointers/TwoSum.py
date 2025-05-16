0248fdcfe3795d501e896054c987247fc4626371
Hoyeon Luke Jang <h.lukejang@gmail.com>
2025-05-15 20:32:19 -0400

    167. Two Sum II - Input Array Is Sorted (reviewed)

--- Two Pointers/TwoSum.py
+++ Two Pointers/TwoSum.py
@@ -6,15 +6,16 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        #two pointer approach to this problem
-
-        left = 0
-        right = len(numbers)-1
+        left, right = 0, len(numbers)-1
 
         while left < right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] < target:
-                left += 1
-            else:
+            two_sum = numbers[left] + numbers[right]
+
+            if two_sum == target:
+                return [left+1,right+1]
+            elif two_sum > target:
                 right -= 1
+            else:
+                left += 1
+            
+        
