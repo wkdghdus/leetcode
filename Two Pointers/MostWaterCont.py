a484cf3db400ae6e77387af80a4f49151178a01f
Hoyeon Luke Jang <h.lukejang@gmail.com>
2025-05-18 18:04:34 -0400

    11. Container With Most Water (reviewed)

--- Two Pointers/MostWaterCont.py
+++ Two Pointers/MostWaterCont.py
@@ -5,19 +5,21 @@ class Solution(object):
         :rtype: int
         """
         
-        #two pointer approach
-        left = 0 
-        right = len(height) - 1
-        maxArea = 0
+        low, high = 0, len(height) - 1
+        max_water = 0
 
-        while left < right:
+        while low < high:
+            curr_water = min(height[low], height[high]) * (high - low)
 
-            currArea = min(height[left], height[right]) * (right - left)
-            maxArea = max(maxArea, currArea)
+            max_water = max(max_water, curr_water)
 
-            if height[left] > height[right]:
-                right -= 1
-            else:
-                left += 1
+            if height[low] < height[high]:
+                low += 1
+            else:               
+                high -= 1
+
+                # note that if the height is the same, 
+                # it is impossible to get a bigger area when you shift either index.
+                # because the width is guaranteed to be shorter and the height will be the same or shorter.  
         
-        return maxArea
+        return max_water
