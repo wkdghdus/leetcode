222a3d77b5c7cc684c8edb2154bc029be4f7a1c8
Hoyeon Luke Jang <h.lukejang@gmail.com>
2025-07-13 18:00:19 -0400

    739. Daily Temperatures (reviewed)

--- Stack/DailyTemp.py
+++ Stack/DailyTemp.py
@@ -8,13 +8,14 @@ class Solution(object):
         res = [0] * len(temperatures)
         stack = []  #temp, ind
 
-        #enumerate through temp since we need the index and the value information. this can be replaced using traditional for loop and using index.
         for ind, temp in enumerate(temperatures):
-            
-            #while the stack is not empty and current temperature is greater than the previous one, keep popping and update the result.
-            while stack and temp > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = ind - stackInd
+
+            while (stack and temp > stack[-1][0]):
+                
+                stack_temp, stack_ind = stack.pop()
+                res[stack_ind] = ind - stack_ind 
+
+
             stack.append((temp, ind))
         
         return res
